7bea0b87e1649335e285347c3795ab8e10d16208
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-03-08 08:22:43 -0300

    DataAccess-Attributes-Created

--- DB/DataAccess.cs
+++ DB/DataAccess.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DB
 {
     public class DataAccess
     {
+        //Attributes
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataReader reader;
+        public SqlDataReader readerProp
+        {
+            get { return reader; }
+        }
     }
 }
