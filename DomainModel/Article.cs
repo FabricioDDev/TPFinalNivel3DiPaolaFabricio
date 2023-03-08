c0f5e9e76115ecbcd9c1934715bdf0ae76b8a226
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-03-08 07:29:52 -0300

    ArticleAtributes-Completed

--- DomainModel/Article.cs
+++ DomainModel/Article.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace DomainModel
 {
-    internal class Article
+    public class Article
     {
+        //Atributes
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public Double Price { get; set; }
     }
 }
