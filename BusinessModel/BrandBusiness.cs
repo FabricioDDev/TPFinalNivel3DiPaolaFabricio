fd584c8300165e4bf11b9ff40a062647b844c01b
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-03-09 07:21:19 -0300

    BrandBusiness-ListingFunction-Completed

--- BusinessModel/BrandBusiness.cs
+++ BusinessModel/BrandBusiness.cs
@@ -14,5 +14,27 @@ namespace BusinessModel
         public BrandBusiness() { data = new DataAccess(); }
         //Attributes
         private DataAccess data;
+
+        //Methods
+        public List<Brand> Listing()
+        {
+            List<Brand> list = new List<Brand>();
+            try
+            {
+                data.Query("select Id, Descripcion from MARCAS");
+                data.Read();
+                while (data.readerProp.Read())
+                {
+                    Brand aux = new Brand(); 
+                    aux.Id = (int)data.readerProp["Id"];
+                    aux.Name = (string)data.readerProp["Descripcion"];
+                    list.Add(aux);
+                }
+                return list;
+            }
+            catch (Exception ex){ throw ex; }
+            finally { }
+        };
+
     }
 }
