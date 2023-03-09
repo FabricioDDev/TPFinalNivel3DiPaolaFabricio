20238f64f9baed274325c3b00c26d49525b2e0d7
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-03-09 07:24:48 -0300

    Attributes-and-Builder-CategoryBusiness-Completed

--- BusinessModel/CategoryBusiness.cs
+++ BusinessModel/CategoryBusiness.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainModel;
+using DB;
 
 namespace BusinessModel
 {
-    internal class CategoryBusiness
+    public class CategoryBusiness
     {
+        //Builder
+        public CategoryBusiness()
+        {
+            data = new DataAccess();
+        }
+        //Attributes
+        private DataAccess data;
     }
 }
