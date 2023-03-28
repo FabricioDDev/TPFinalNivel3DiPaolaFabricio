c81eecb2e3858899f4e556d13538c2d115c552f5
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-03-28 07:24:14 -0300

    FavoriteBusiness-ListingFunction-Completed

--- BusinessModel/FavoriteBusiness.cs
+++ BusinessModel/FavoriteBusiness.cs
@@ -13,6 +13,21 @@ namespace BusinessModel
         public FavoriteBusiness() { data = new DataAccess(); }
         private DataAccess data;
 
-        
+        public List<Favorites> Listing(int IdU)
+        {
+            List<Favorites> list = new List<Favorites>();
+            data.Query("select Id, IdUser, IdArticulo from FAVORITOS where IdUser = IdU ");
+            data.Parameters("IdU", IdU);
+            data.Read();
+            while (data.readerProp.Read())
+            {
+                Favorites aux = new Favorites();
+                aux.Id = (int)data.readerProp["Id"];
+                aux.IdUser = (int)data.readerProp["IdUser"];
+                aux.IdArticle = (int)data.readerProp["IdArticulo"];
+                list.Add(aux);
+            }
+            return list;
+        }
     }
 }
