8a83cb90fe6645b7401c077e6e3b2105243e0dd8
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-04-01 15:48:00 -0300

    FrmUserConfig-Attribute-Created

--- EcommerceWebApp/FrmUserConfig.aspx.cs
+++ EcommerceWebApp/FrmUserConfig.aspx.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DomainModel;
+using BusinessModel;
 
 namespace EcommerceWebApp
 {
     public partial class UserConfig : System.Web.UI.Page
     {
+        private static User user;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            user = (User)Session["activeUser"];
         }
     }
 }
