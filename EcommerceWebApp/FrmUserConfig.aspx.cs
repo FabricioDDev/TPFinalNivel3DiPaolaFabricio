b012cb1b3ddd3c26dc02587841652fa25e909cd2
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-04-02 10:47:37 -0300

    chargeControlls-FrmUserConfig-Completed

--- EcommerceWebApp/FrmUserConfig.aspx.cs
+++ EcommerceWebApp/FrmUserConfig.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using DomainModel;
 using BusinessModel;
+using System.IO;
 
 namespace EcommerceWebApp
 {
@@ -15,6 +16,34 @@ namespace EcommerceWebApp
         protected void Page_Load(object sender, EventArgs e)
         {
             user = (User)Session["activeUser"];
+            if (!IsPostBack)
+            {
+                chargeControlls();
+            }
+        }
+        private void chargeControlls()
+        {
+            TxtEmail.Text = user.EmailProperty;
+            TxtPassword.Text = user.PassProperty;
+            TxtName.Text = user.nameProperty;
+            TxtLastName.Text = user.lastNameProperty;
+            chargeImage();
+        }
+        private void chargeImage()
+        {
+            if (user.UrlProfileImage.Contains("https"))
+            {
+                ImgArticle.ImageUrl = user.UrlProfileImage;
+                TxtUrl.Text = user.UrlProfileImage;
+            }
+            else if (File.Exists(MapPath("~/Images/Articles/Article-" + user.idProperty.ToString()+ ".jpg")))
+            {
+                ImgArticle.ImageUrl = "./Images/Articles/Article-" + user.idProperty.ToString() + ".jpg";
+            }
+            else
+            {
+                ImgArticle.ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png";
+            }
         }
     }
 }
